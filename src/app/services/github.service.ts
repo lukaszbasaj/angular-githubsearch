48f3f02fab524400f278a1358792501139412e4f
Łukasz Basaj <lbasaj@gmail.com>
2019-04-11 22:59:05 +0200

    some refactoring

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -8,17 +8,18 @@ export class GithubService {
   private username = 'lukaszbasaj';
   private client_id = '949e8ea08bace58ff7ae';
   private client_secret = 'a0c25c4972fce5af853d6e8cf374134138bde7c1';
+  private apiUrl = 'https://api.github.com/users/';
 
   constructor(private _http:Http) {
    }
     
    public getUser(){
-     return this._http.get(`https://api.github.com/users/${this.username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+     return this._http.get(`${this.apiUrl}${this.username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
           .map((res => res.json())
           )}
 
     public getRepos(){
-      return this._http.get(`https://api.github.com/users/${this.username}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+      return this._http.get(`${this.apiUrl}${this.username}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
           .map((res => res.json())
           )}
           
