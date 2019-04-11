913476751fbb537da06274c9794b2a7500458c99
Łukasz Basaj <lbasaj@gmail.com>
2019-04-11 17:29:17 +0200

    change layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './components/github/github.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatIconModule, 
-  MatAutocompleteModule, MatChipsModule,
-  MatFormFieldModule,
-  MatToolbarModule, MatSidenavModule
-} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -22,17 +14,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    MatInputModule,
+    HttpModule,  
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MatIconModule, MatInputModule,
-    MatAutocompleteModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatToolbarModule, 
-    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
