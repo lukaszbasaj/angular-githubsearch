9373123da035e2767b6976c94e5a555f32332ffc
Łukasz Basaj <lbasaj@gmail.com>
2019-04-11 22:28:35 +0200

    initial commit for rxjs appying

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Username } from '../username.model'
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
 
-  private username = 'lukaszbasaj';
+  private username:Username[];
   private client_id = '949e8ea08bace58ff7ae';
   private client_secret = 'a0c25c4972fce5af853d6e8cf374134138bde7c1';
+  headers: HttpHeaders;  
+  options: any;
+  apiUrl = 'https://api.github.com/users/'
 
-  constructor(private _http:Http) {
+  constructor(req: HttpRequest<any>, next: HttpHandler) {
    }
     
-   public getUser(){
-     return this._http.get(`https://api.github.com/users/${this.username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+   public getUser():Observable<Username[]>{
+     return this._http.get<Username[]>(`${this.apiUrl}${this.username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
           .map((res => res.json())
           )}
 
     public getRepos(){
-      return this._http.get(`https://api.github.com/users/${this.username}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+      return this._http.get(`${this.apiUrl}${this.username}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
           .map((res => res.json())
           )}
           
